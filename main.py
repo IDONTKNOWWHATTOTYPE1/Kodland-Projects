14b1a6e563873b63eceb549c44cf661ea39a03f1
HELLO450 <149254365+IDONTKNOWWHATTOTYPE1@users.noreply.github.com>
2023-10-28 17:32:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+meme_dict = {
+            "IDK": "I dont know Non lo so",
+            "XD": "uguale a lol ma espresso in una faccina",
+            "nvm": "Never Mind Non Importa",
+            }
+            
